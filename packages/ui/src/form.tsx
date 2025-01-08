1a41309714e5e1cbe8ea09af3bd184716ecc9eb1
Silvain Raveau <silvain@raveau.net>
2025-01-08 18:20:00 +0100

    refactor: simplify form handling by removing zodResolver and restructuring context types

--- packages/ui/src/form.tsx
+++ packages/ui/src/form.tsx
@@ -1,40 +1,22 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers/zod';
 import type * as LabelPrimitive from '@radix-ui/react-label';
 import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
-import type { ControllerProps, FieldPath, FieldValues, UseFormProps } from 'react-hook-form';
-import { useForm as __useForm, Controller, FormProvider, useFormContext } from 'react-hook-form';
-import type { ZodType, ZodTypeDef } from 'zod';
-
-import { cn } from './lib/utils';
+import type { ControllerProps, FieldPath, FieldValues } from 'react-hook-form';
+import { Controller, FormProvider, useFormContext } from 'react-hook-form';
 
 import { Label } from './label';
+import { cn } from './lib/utils';
 
-const useForm = <TOut extends FieldValues, TDef extends ZodTypeDef, TIn extends FieldValues>(
-  props: Omit<UseFormProps<TIn>, 'resolver'> & {
-    schema: ZodType<TOut, TDef, TIn>;
-  },
-) => {
-  const form = __useForm<TIn, unknown, TOut>({
-    ...props,
-    resolver: zodResolver(props.schema, undefined),
-  });
-
-  return form;
-};
-
-const Form = FormProvider;
-
-interface FormFieldContextValue<
+type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> {
+> = {
   name: TName;
-}
+};
 
-const FormFieldContext = React.createContext<FormFieldContextValue | null>(null);
+const FormFieldContext = React.createContext<FormFieldContextValue>({} as FormFieldContextValue);
 
 const FormField = <
   TFieldValues extends FieldValues = FieldValues,
@@ -54,11 +36,7 @@ const useFormField = () => {
   const itemContext = React.useContext(FormItemContext);
   const { getFieldState, formState } = useFormContext();
 
-  if (!fieldContext) {
-    throw new Error('useFormField should be used within <FormField>');
-  }
   const fieldState = getFieldState(fieldContext.name, formState);
-
   const { id } = itemContext;
 
   return {
@@ -71,9 +49,9 @@ const useFormField = () => {
   };
 };
 
-interface FormItemContextValue {
+type FormItemContextValue = {
   id: string;
-}
+};
 
 const FormItemContext = React.createContext<FormItemContextValue>({} as FormItemContextValue);
 
@@ -151,6 +129,4 @@ const FormMessage = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<
 );
 FormMessage.displayName = 'FormMessage';
 
-export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, useForm, useFormField };
-
-export { useFieldArray } from 'react-hook-form';
+export { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, FormProvider, useFormField };
--- packages/ui/src/label.tsx
+++ packages/ui/src/label.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as LabelPrimitive from '@radix-ui/react-label';
 import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
