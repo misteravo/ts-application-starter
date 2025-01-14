439109cdaa341893751ff29acd1da60160dbf2af
Silvain Raveau <silvain@raveau.net>
2025-01-14 21:53:33 +0100

    feat: redirect unauthenticated users from dashboard page to sign-in page

--- application/nextjs/src/app/(dashboard)/page.tsx
+++ application/nextjs/src/app/(dashboard)/page.tsx
@@ -1,5 +1,16 @@
-'use client';
+import { getCurrentSession } from '@acme/backend';
 
-export default function DashboardPage() {
+import { get2FARedirect } from '@acme/backend';
+import { redirect } from 'next/navigation';
+
+export default async function DashboardPage() {
+  const { session, user } = await getCurrentSession();
+  if (session === null) {
+    return redirect('/sign-in');
+  }
+  if (user.registered2FA && !session.twoFactorVerified) {
+    return redirect(get2FARedirect(user));
+  }
+
   return <h1 className="text-2xl font-bold">Welcome to your dashboard</h1>;
 }
