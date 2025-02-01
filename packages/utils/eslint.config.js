d21658233e268e35022af7df6c98eb01ca135e2c
Silvain Raveau <silvain@raveau.net>
2025-02-01 12:04:01 +0100

    chore: configure ESLint to ignore dist and node_modules directories

--- packages/utils/eslint.config.js
+++ packages/utils/eslint.config.js
@@ -3,7 +3,15 @@ import baseConfig from '@acme/eslint-config/base';
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ['dist/**/*', 'node_modules/**/*'],
   },
   ...baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
 ];
