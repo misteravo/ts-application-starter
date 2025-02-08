bff00e8c2a1db060db35f1b846b10374c25cf38f
Silvain Raveau <silvain@raveau.net>
2025-02-08 21:24:57 +0100

    feat: enhance deployment script with remote server support

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -11,24 +11,35 @@ const isReload = Boolean(argv.reload);
 
 if (isStart === isReload) throw new Error('Either --start or --reload must be provided');
 
-const destination = process.env.PRODUCTION_DESTINATION;
+if (!process.env.PRODUCTION_DESTINATION) throw new Error('PRODUCTION_DESTINATION is not set');
+
+const destinationTab = process.env.PRODUCTION_DESTINATION.split(':');
+const fullDestinationPath = process.env.PRODUCTION_DESTINATION;
+
+const server = destinationTab.length == 2 ? destinationTab[0] : null;
+const destinationPath = destinationTab.length == 2 ? destinationTab[1] : fullDestinationPath;
+
 const nextJsPath = 'application/nextjs';
 const applicationName = 'ts-application-starter';
 
 await $`pnpm build`;
-await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/public ${destination}/`;
-await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/.next/standalone/ ${destination}/`;
-await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/.next/static ${destination}/.next/`;
+await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/public ${fullDestinationPath}/`;
+await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/.next/standalone/ ${fullDestinationPath}/`;
+await $`rsync -av --exclude-from=".rsyncignore" --delete-after ${nextJsPath}/.next/static ${fullDestinationPath}/.next/`;
 
 if (isStart) {
-  await $`rsync -av --delete-after .env ${destination}/`;
-  await $`rsync -av --delete-after sqlite.db ${destination}/`;
+  await $`rsync -av --delete-after .env ${fullDestinationPath}/`;
+  await $`rsync -av --delete-after sqlite.db ${fullDestinationPath}/`;
 }
 
 if (isStart) {
-  await $`cd ${destination} && pm2 start -i --time --name ${applicationName} node -- --env-file=.env ${nextJsPath}/server.js`;
+  const command = `cd ${destinationPath} && pm2 start -i --time --name ${applicationName} node -- --env-file=.env ${nextJsPath}/server.js`;
+  if (server) await $`ssh ${server} "${command}"`;
+  else await $`${command}`;
 }
 
 if (isReload) {
-  await $`cd ${destination} && pm2 reload --time ${applicationName}`;
+  const command = `cd ${destinationPath} && pm2 reload --time ${applicationName}`;
+  if (server) await $`ssh ${server} "${command}"`;
+  else await $`${command}`;
 }
