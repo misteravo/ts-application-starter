6f5ef9f5a8d3764f36ac93fa6380c799e47e9571
Silvain Raveau <silvain@raveau.net>
2025-05-26 11:41:29 +0200

    refactor: extract common action handling logic into runAction for improved code organization and reuse

--- application/nextjs/src/lib/safe-action.ts
+++ application/nextjs/src/lib/safe-action.ts
@@ -6,30 +6,28 @@ type ErrorMessage = {
   message: string;
 };
 
-export function schemaAction<S extends Schema, R>(schema: S, actionFn: (props: z.infer<S>) => Promise<R>) {
+type ActionFunction<S extends Schema, R> = (props: z.infer<S>) => Promise<R>;
+
+export function schemaAction<S extends Schema, R>(schema: S, actionFn: ActionFunction<S, R>) {
   return async function (props: z.infer<S>): Promise<R | ErrorMessage> {
-    try {
-      if (!(await globalPOSTRateLimit())) return { message: 'Too many requests' };
-      const [data] = await safeTry(schema.parseAsync(props));
-      if (!data) return { message: 'Invalid or missing fields' };
-      return actionFn(data as z.infer<S>);
-    } catch (error) {
-      if (error instanceof Error) return { message: error.message };
-      return { message: 'Unknown error' };
-    }
+    return runAction(schema, props, actionFn);
   };
 }
 
-export function formAction<S extends Schema, R>(schema: S, actionFn: (props: z.infer<S>) => Promise<R>) {
+export function formAction<S extends Schema, R>(schema: S, actionFn: ActionFunction<S, R>) {
   return async function (_prev: R, formData: FormData): Promise<R | ErrorMessage> {
-    try {
-      if (!(await globalPOSTRateLimit())) return { message: 'Too many requests' };
-      const [data] = await safeTry(schema.parseAsync(formData));
-      if (!data) return { message: 'Invalid or missing fields' };
-      return actionFn(data as z.infer<S>);
-    } catch (error) {
-      if (error instanceof Error) return { message: error.message };
-      return { message: 'Unknown error' };
-    }
+    return runAction(schema, formData, actionFn);
   };
+}
+
+async function runAction<S extends Schema, R>(schema: S, props: unknown, actionFn: ActionFunction<S, R>) {
+  try {
+    if (!(await globalPOSTRateLimit())) return { message: 'Too many requests' };
+    const [data] = await safeTry(schema.parseAsync(props));
+    if (!data) return { message: 'Invalid or missing fields' };
+    return actionFn(data as z.infer<S>);
+  } catch (error) {
+    if (error instanceof Error) return { message: error.message };
+    return { message: 'Unknown error' };
+  }
 }
