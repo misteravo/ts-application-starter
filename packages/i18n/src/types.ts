60c7db2c5239f07ab363eb8ae14e0a91427be59d
Silvain Raveau <silvain@raveau.net>
2025-06-13 19:19:16 +0200

    refactor(i18n): enhance Translations type to support multiple non-English languages

--- packages/i18n/src/types.ts
+++ packages/i18n/src/types.ts
@@ -2,4 +2,6 @@ export const supportedLanguages = ['en', 'fr'] as const;
 
 export type LanguageCode = (typeof supportedLanguages)[number];
 
-export type Translations<K extends string> = Record<K, { fr: string }>;
+type NonEnglishLanguageCode = Exclude<LanguageCode, 'en'>;
+
+export type Translations<K extends string> = Record<K, Record<NonEnglishLanguageCode, string>>;
